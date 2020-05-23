19ecf745c823e6dcfb1e08ed0dea42aebd7a6edb
Vector Le <vectorle@protonmail.com>
2020-05-23 10:06:33 -0700

    added alt text on logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
 	return <div id="navbar">
 		<nav>
-			<img src="./img/logo.png"/>
+			<img src="./img/logo.png" alt="logo"/>
 			<ul>
 				<li>Shop</li>
 	            <li>About</li>
