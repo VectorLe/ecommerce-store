51fbb02cee3d081462bdfe149f0228ec89d6c5c8
Vector Le <vectorle@protonmail.com>
2020-05-24 15:03:07 -0700

    Updates

---
+++ precache-manifest.b1968418d07d391026ab90fd635bfdc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a84d038273b0c0f414504b96ffda92",
+    "url": "/ecommerce-store/index.html"
+  },
+  {
+    "revision": "61a6cd40b824c749b324",
+    "url": "/ecommerce-store/static/css/main.d86bbc87.chunk.css"
+  },
+  {
+    "revision": "2d9d860c90ae3d1f6455",
+    "url": "/ecommerce-store/static/js/2.8c33661a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce-store/static/js/2.8c33661a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a6cd40b824c749b324",
+    "url": "/ecommerce-store/static/js/main.89ca0669.chunk.js"
+  },
+  {
+    "revision": "43d192d4fbf72503b31e",
+    "url": "/ecommerce-store/static/js/runtime-main.9149f25e.js"
+  }
+]);
